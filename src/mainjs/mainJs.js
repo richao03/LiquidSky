85eb5fd43ec16df1cf3d3c4fcf1fb5fd18fb17c1
Richard Chao <r.chao03@gmail.com>
2017-02-16 12:59:13 -0500

    midas lend me your finger

--- src/mainjs/mainJs.js
+++ src/mainjs/mainJs.js
@@ -179,43 +179,49 @@ next()
 
 
   new ScrollMagic.Scene({ triggerElement: "#sec1", duration: 5200, offset: 60 })
-      .setClassToggle("#high1", "active") 
+      .setClassToggle("#high1", "active")
       .addTo(controller);
-        new ScrollMagic.Scene({ triggerElement: "#sec1half"})
-      .setClassToggle("#high2", "active") 
+  new ScrollMagic.Scene({ triggerElement: "#sec1half" })
+      .setClassToggle("#high2", "active")
       .addTo(controller);
 
   new ScrollMagic.Scene({ triggerElement: "#sec2" })
-      .setClassToggle("#high3", "active") 
+      .setClassToggle("#high3", "active")
       .addTo(controller);
-                 new ScrollMagic.Scene({ triggerElement: "#sec2half"})
-      .setClassToggle("#high4", "active") 
+  new ScrollMagic.Scene({ triggerElement: "#sec2half" })
+      .setClassToggle("#high4", "active")
       .addTo(controller);
   new ScrollMagic.Scene({ triggerElement: "#sec3" })
-      .setClassToggle("#high5", "active") 
+      .setClassToggle("#high5", "active")
       .addTo(controller);
-             new ScrollMagic.Scene({ triggerElement: "#sec3half"})
-      .setClassToggle("#high6", "active") 
+  new ScrollMagic.Scene({ triggerElement: "#sec3half" })
+      .setClassToggle("#high6", "active")
       .addTo(controller);
+  new ScrollMagic.Scene({ triggerElement: "#sec4" })
+      .setClassToggle("#high7", "neon")
+      .addTo(controller);
+  new ScrollMagic.Scene({ triggerElement: "#sec4" })
+      .setClassToggle("#high8", "neon")
+      .addTo(controller);
+  new ScrollMagic.Scene({ triggerElement: "#sec4" })
+      .setClassToggle("#high9", "neon")
+      .addTo(controller);
+  // var elements = ['#high7','#high8','#high9'];
 
 
 
+  // var setMultipleClasses = TweenMax.set(elements, {
+  //     className: "neon"
+  // });
 
-var elements = ['#high7','#high8','#high9'];
+  // // Create a ScrollMagic Scene
+  // new ScrollMagic.Scene({
+  //   reverse:false,
+  //   triggerElement: "#sec4"
+  // })
+  // .setTween(setMultipleClasses)
+  // .addTo(controller);
 
-
-
-var setMultipleClasses = TweenMax.set(elements, {
-    className: "neon"
-});
-
-// Create a ScrollMagic Scene
-new ScrollMagic.Scene({
-  reverse:false,
-  triggerElement: "#sec4"
-})
-.setTween(setMultipleClasses)
-.addTo(controller);
 $.fn.moveIt = function(){
   var $window = $(window);
   var instances = [];
